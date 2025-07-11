22ad54a5982cca63afc2bafb7a0b8bed5157223f
Rishi Sahu <rishisahu4204@gmail.com>
2025-07-11 22:20:33 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,19 @@
+#Task 1: Perform Basic Mathematical Operations
+
+
+a=(float(input("Enter the 1st no: ")))
+b=(float(input("Enter the 2nd no: ")))
+add=a + b
+print ( "addition: ", add )
+sub= (a - b)
+print ("Substraction: ", sub )
+mul= (a*b)
+print("Multiplication:", mul)
+div = (a/b)
+print("Divide; ", div)
+
+
+#Task 2: Create a Personalized Greeting
+first=(input("Enter your first name: "))
+last=(input("Enter your last name: "))
+print("Hello, ", first + " " + last +" ! Welcome to the Python Program")
